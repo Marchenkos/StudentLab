989628e29ee56531a4f9fbba68433ba1c962ceed
Ksenia <43186251+Marchenkos@users.noreply.github.com>
2019-11-29 17:48:16 +0300

    Add patterns (#3)

---
+++ Patterns/singleton.js
@@ -0,0 +1,38 @@
+let instance = null;
+
+class DataBase {
+    constructor() {
+        if (instance) {
+            return instance;
+        }
+
+        instance = this;
+        this.isConnectionOpened  = false;
+    }
+
+    openConnection() {
+        this.isConnectionOpened  = true;
+    }
+
+    closeConnection() {
+        if(this.isConnectionOpened) {
+            this.isOpenConnection = false;
+        } else {
+            console.log("Connection isn't open");
+        }
+    }
+
+    executeQueries() {
+        this.isOpenConnection ? console.log("Querie is executing") : console.log("Querie failed! Connection isn't open");
+    }
+}
+
+
+let newDataBase = new DataBase();
+console.log(newDataBase.isConnectionOpened);
+newDataBase.openConnection();
+console.log(newDataBase.isConnectionOpened);
+
+let newDataBase2 = new DataBase();
+console.log(newDataBase2.isConnectionOpened);
+newDataBase.closeConnection();
