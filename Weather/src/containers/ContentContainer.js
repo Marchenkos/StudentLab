a4671b717d3b9c25fbffb25b37ae1fdfd049629c
Ksenia <43186251+Marchenkos@users.noreply.github.com>
2020-01-17 20:43:19 +0300

    remove extra brackets

--- Weather/src/containers/ContentContainer.js
+++ Weather/src/containers/ContentContainer.js
@@ -2,7 +2,7 @@ import { connect } from "react-redux";
 import Content from "../components/Content";
 import { selectCityNameSelector, selectSearchModeSelector } from "../selectors/selector";
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
     return {
         cityName: selectCityNameSelector(state),
         searchMode: selectSearchModeSelector(state),
